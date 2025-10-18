460e512374885b5b929daee64d06ec909d2c05de
matt4110 <matthew.johnson4110@gmail.com>
2025-10-18 18:32:53 -0400

    added sniffer functionality

--- sniffer-ipheader-decode.py
+++ sniffer-ipheader-decode.py
@@ -4,6 +4,9 @@ import socket
 import struct
 import sys
 
+# host to listen on
+HOST = sys.argv[1]
+
 class IP:
     def __init__(self, buff=None):
         header = struct.unpack('<BBHHHBBH4s4s', buff)
@@ -111,6 +114,32 @@ class ICMP:
             if os.name == 'nt':
                 sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
              
+def main():
+    # Check OS and set appropriate protocol
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    # Create raw socket and bind to public interace
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, 0))
+
+    # include the IP header in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    # if we're on Windows, turn on promiscuous mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    # read one packet
+    print(sniffer.recvfrom(65565))
+
+    # if we're on Windows, turn off promiscuous mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         host = sys.argv[1]
