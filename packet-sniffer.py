9d92005a21ed5aaf21473f815997721a567dc29f
matt4110 <matthew.johnson4110@gmail.com>
2025-10-18 17:30:40 -0400

    debuggin

--- packet-sniffer.py
+++ packet-sniffer.py
@@ -1,25 +1,30 @@
 # This tools needs root/sys admin privileges to run
 # Usage: python3 packet-sniffer.py [your machine's IP Addr]
 
-import socket
+from ctypes import *
 import os
+import socket
+import struct
 import sys
 
 # host to listen on
 HOST = sys.argv[1]
 
 def main():
-    # create raw socket, bin to public interface
+    # Check OS and set appropriate protocol
     if os.name == 'nt':
         socket_protocol = socket.IPPROTO_IP
     else:
         socket_protocol = socket.IPPROTO_ICMP
 
+    # Create raw socket and bind to public interace
     sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
     sniffer.bind((HOST, 0))
+
     # include the IP header in the capture
     sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
+    # if we're on Windows, turn on promiscuous mode
     if os.name == 'nt':
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
