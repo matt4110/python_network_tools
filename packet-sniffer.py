72d0f320a2f05ada3af9255346596290e8de6b2f
matt4110 <matthew.johnson4110@gmail.com>
2026-04-02 00:04:50 -0400

    printing the whole header now

--- packet-sniffer.py
+++ packet-sniffer.py
@@ -10,6 +10,18 @@ import sys
 # host to listen on
 HOST = sys.argv[1]
 
+def parse_ip_header(data):
+    # Unpack the first 20 bytes of the IP header
+    ip_header = struct.unpack('!BBHHHBBH4s4s', data[:20])
+    version_ihl = ip_header[0]
+    version     = version_ihl >> 4
+    ihl         = (version_ihl & 0xF) * 4  # Header length in bytes
+    ttl         = ip_header[5]
+    protocol    = ip_header[6]
+    src_ip      = socket.inet_ntoa(ip_header[8])
+    dst_ip      = socket.inet_ntoa(ip_header[9])
+    print(f"{version} {ihl} {ttl} {protocol} {src_ip} {dst_ip}")
+
 def main():
     # Check OS and set appropriate protocol
     if os.name == 'nt':
@@ -28,18 +40,21 @@ def main():
     if os.name == 'nt':
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-    print("Waiting for packets...")
+    print("Version | Header Len | TTL | Protocol | Src IP | Dest IP | Payload")
 
     # read packets
     while True:
         try:
             packets = sniffer.recvfrom(65565)
-            print(packets[0].decode('ascii', errors='ignore'), packets[1])
+            parse_ip_header(packets[0])
+            print(packets[0][20:].decode('ascii', errors='ignore'))
+            
         except KeyboardInterrupt:
              # if we're on Windows, turn off promiscuous mode
             if os.name == 'nt':
                 sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
             sys.exit(0)
+
         except Exception as ex:
             print(f"Error: {ex}")
 
